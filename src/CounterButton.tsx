089f446d62078f4b29527c1af416450c05155e6d
bquan-cgi <bo.quan@cgi.com>
2024-05-17 13:22:30 +0200

    expose sample remote button

--- src/CounterButton.tsx
+++ src/CounterButton.tsx
@@ -6,8 +6,8 @@ export default function CounterButton() {
     const [count, setCount] = useState(0)
 
     return (
-        <div className="bg-amber-300">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <div className="bg-amber-300 px-2">
+        <button className='bg-green-200 w-36' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
