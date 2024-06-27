83ecaf23ce1d6bd529a23abb16c5f71d5c4f7800
bquan-cgi <bo.quan@cgi.com>
2024-06-27 13:50:18 +0200

    add propTypes, not working

--- src/About.tsx
+++ src/About.tsx
@@ -1,18 +1,21 @@
-import CounterButton1 from "app1/CounterButton";
+import CounterButton from "app1/CounterButton";
+// import CounterButton1 from "app1/CounterButton1";
+const CounterButton1 = React.lazy(() => import('app1/CounterButton1'))
 import useCount from "app1/store";
+import React from "react";
 
 export default function About() {
   const [count, setCount] = useCount();
   return (
     <>
       <div className="container mx-auto px-4 bg-yellow-500">about us</div>
-      <CounterButton1 name="fdgd"></CounterButton1>
+      <CounterButton1 greeting="rgrg" gd="hi"></CounterButton1>
 
       <h3>Button in shell app</h3>
       <button
         type="button"
         className="blue-button"
-        onClick={() => setCount((count) => count + 1)}
+        onClick={() => setCount((count: number) => count + 1)}
       >
         count is {count}
       </button>
--- src/remoteTypes.d.ts
+++ src/remoteTypes.d.ts
@@ -7,11 +7,29 @@ declare module "app1/App1Layout" {
 	export default MicrofrontendOne
 }
 
-declare module "app3/App2Home" {
+declare module "app1/store";
+declare module "app1/CounterButton";
+
+declare module "app1/CounterButton1" {
+	import { ComponentType } from "react";
+	interface CounterButtonProps {
+
+		greeting : string
+		gd : string
+	}
+
+	const cb1: ComponentType<CounterButtonProps>;
+	export default cb1;
+}
+
+declare module "app2/App2Home" {
 	import { ComponentType } from "react";
 	interface Props {}
 	const MicrofrontendTwo: ComponentType<Props>;
 	export default MicrofrontendTwo;
+}
+
+declare module "app2/App2Content" {
 }
 
 declare module "app3/App3Home" {
